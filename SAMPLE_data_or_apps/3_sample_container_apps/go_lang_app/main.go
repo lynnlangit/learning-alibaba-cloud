7a286f63f19f1f8a215b70e9a2048a1eedffe6a0
Lynn Langit <lynnlangit@live.com>
2020-02-11 10:23:36 -0800

    Update main.go

--- SAMPLE_data_or_apps/3_sample_container_apps/go_lang_app/main.go
+++ SAMPLE_data_or_apps/3_sample_container_apps/go_lang_app/main.go
@@ -14,17 +14,14 @@ func main() {
                 port = fromEnv
         }
 
-        // register hello function to handle all requests
         server := http.NewServeMux()
         server.HandleFunc("/", hello)
-
-        // start the web server on port and accept requests
+        
         log.Printf("Server listening on port %s", port)
         err := http.ListenAndServe(":"+port, server)
         log.Fatal(err)
 }
 
-// hello responds to the request with a plain-text "Hello, world" message.
 func hello(w http.ResponseWriter, r *http.Request) {
         log.Printf("Serving request: %s", r.URL.Path)
         host, _ := os.Hostname()
