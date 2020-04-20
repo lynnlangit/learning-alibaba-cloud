24817740e96cde752cf505b9ac2ade3c4fe75e19
Lynn Langit <lynnlangit@live.com>
2020-04-20 10:11:34 -0500

    Update server.js

--- SAMPLE_data_or_apps/3_sample_container_apps/node_app/server.js
+++ SAMPLE_data_or_apps/3_sample_container_apps/node_app/server.js
@@ -4,7 +4,7 @@ var port = 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World\n')
+  response.end('Hello Alibaba Cloud World\n')
 })
 
 server.listen(port)
